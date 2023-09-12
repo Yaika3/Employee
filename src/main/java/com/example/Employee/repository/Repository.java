aafa40754ae15c7e715155f007f1e3b01ce7a619
Yaika3 <kabinidi@mail.ru>
2023-09-12 18:30:55 +0300

    1 com

--- src/main/java/com/example/Employee/repository/Repository.java
+++ src/main/java/com/example/Employee/repository/Repository.java
@@ -2,8 +2,16 @@ package com.example.Employee.repository;
 
 import com.example.Employee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 @org.springframework.stereotype.Repository
 public interface Repository extends JpaRepository<Employee, Integer> {
+    //нужен ли GROUP BY
+    @Query(value = "SELECT AVG(Salary) FROM Employee HAVING MIN(department) > 1" , nativeQuery = true)
+    @Query(value = "SELECT MAX(Salary) FROM Employee HAVING MIN(department) > 1" , nativeQuery = true)
+
+    List<Employee> getAVGEmployee (){};
 }
 
